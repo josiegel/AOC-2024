97787dd9b4e45aed82c62bd456c3e12f6f97d328
Jonah Siegel <102381719+josiegel@users.noreply.github.com>
2024-12-07 01:13:48 -0500

    Add files via upload

---
+++ aoc 2024 day 7 part 2.py
@@ -0,0 +1,32 @@
+outputs = []
+inputs = []
+
+with open('input.txt', 'r') as file:
+    for line in file:
+        outputs.append(int(line.split(':')[0]))
+        inputs.append(line.split(':')[1].split())
+
+# fun with recursion
+def possible_totals(ls):
+    if len(ls) == 2:
+        return [ls[0] + ls[1], ls[0] * ls[1], int(str(ls[0]) + str(ls[1]))]
+    else:
+        ans = []
+        tail = ls.pop()
+        for num in possible_totals(ls):
+            ans.append(tail * num)
+            ans.append(tail + num)
+            ans.append(int(str(num) + str(tail)))
+        return ans
+
+def test_line(output, nums):
+    int_nums = list(map(int, nums))
+    if output in possible_totals(int_nums):
+        return output
+    else:
+        return 0
+
+total = 0        
+for i in range(len(outputs)):
+    total += test_line(outputs[i], inputs[i])
+print(total)
