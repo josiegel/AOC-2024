b523b6c87b33aecb4a13ccc8012a5a186f50baf8
Jonah Siegel <102381719+josiegel@users.noreply.github.com>
2024-12-19 02:15:00 -0500

    Add files via upload

---
+++ aoc 2024 day 18 part 2.py
@@ -0,0 +1,54 @@
+import time
+input = 'input.txt'
+drops = 1024
+size = 71
+
+def initialize_bytes(input):
+    bytes = []
+    with open(input, 'r') as file:
+        for line in file:
+            bytes.append(tuple(list(map(int,line.strip().split(",")))))
+    return bytes
+
+def get_next_node(queue,scores):
+    next_node = queue[0]
+    for node in queue:
+        if node in scores and scores[node] < scores[next_node]:
+            next_node = node
+    return next_node
+
+def get_steps(start, finish, walls):
+    queue = []
+    node_scores = {}
+    for i in range(0,size):
+        for j in range(0,size):
+            if (i,j) not in walls:
+                node_scores[(i,j)] = float('inf')
+                queue.append((i,j))
+    node_scores[start] = 0
+    while queue:
+        current = get_next_node(queue, node_scores)
+        for point in [(current[0]+1, current[1]),(current[0]-1, current[1]),(current[0], current[1]+1),(current[0], current[1]-1)]:
+            if point in node_scores and node_scores[point] > 1 + node_scores[current]:
+                node_scores[point] = 1+node_scores[current]
+        queue.remove(current)
+    return node_scores[finish]
+
+
+def main():
+    start = (0,0)
+    finish = (size-1, size-1)
+    bytes = initialize_bytes(input)
+    walls = bytes[:1024]
+    bytes = bytes[1024:]
+    steps = 0
+    while steps < float('inf'):
+        walls.append(bytes[0])
+        bytes = bytes[1:]
+        steps = get_steps(start, finish, walls)
+        print(len(walls), " bytes takes", steps, "steps!")
+
+start_time = time.time()    
+main()
+end_time = time.time()
+print(end_time - start_time, " seconds")
